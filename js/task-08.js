ee1143e3b841607b4c80a133c6b1f3574247cd51
Anastasia-Aleksandrova <a-a2018@ukr.net>
2022-04-29 18:32:55 +0300

    fixed 6 and 8 tasks

--- js/task-08.js
+++ js/task-08.js
@@ -6,16 +6,19 @@ refs.form.addEventListener("submit", onFormSubmit);
 
 function onFormSubmit(event) { 
   event.preventDefault();
+  
 
   const {
       elements: { email, password }
-    } = event.currentTarget;
+  } = event.currentTarget;
 
-    if (email.value === "" || password.value === "") {
-      return alert("Bсе поля должны быть заполнены!");
-    }
+  if (email.value === "" || password.value === "") {
+    return alert("Bсе поля должны быть заполнены!");
+  }
 
-  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  // console.log(`Email: ${email.value}, Password: ${password.value}`);
+  console.log(`{Email: ${email.value}, Password: ${password.value}}`);
+ 
   
   event.currentTarget.reset();
 }
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,31 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  controls: document.querySelector("#controls"),
+  createBtn: document.querySelector("[data-create]"),
+  destroyBtn: document.querySelector("[data-destroy]"),
+  boxes: document.querySelector("#boxes"),
+};
+
+refs.createBtn.addEventListener("click", createBoxes);
+
+
+let i = 0;
+
+function createBoxes(amount) {
+  do {
+    onClickButton();
+  } while (i < amount);
+}
+  
+function onClickButton() { 
+  i += 1;
+  const markup = document.createElement("div");
+  
+  markup.style.backgroundColor = getRandomHexColor();
+  markup.style.width = "30px";
+  markup.style.height = "30px";
+  refs.boxes.append(markup);
+};
